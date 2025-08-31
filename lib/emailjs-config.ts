70094af1f132f273c2b397ca967afae6d784b112
devr01499-ui <devr01499@gmail.com>
2025-08-31 11:06:36 +0530

    Updated EmailJS configuration with actual service ID and template ID

--- lib/emailjs-config.ts
+++ lib/emailjs-config.ts
@@ -3,15 +3,15 @@
 
 export const EMAILJS_CONFIG = {
   // Environment variables for EmailJS credentials
-  SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || 'service_admirerx',
-  TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || 'template_admirerx_contact',
+  SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || 'service_n9ihigq',
+  TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || 'template_gkhcy08',
   PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || 'x9Mlm_5Uv0iRxcxdN',
   TO_EMAIL: process.env.NEXT_PUBLIC_EMAILJS_TO_EMAIL || 'devr01499@gmail.com'
 }
 
 // EmailJS Template Structure (to be created in EmailJS dashboard):
 /*
-Template Name: template_admirerx_contact
+Template Name: template_gkhcy08
 Template Subject: New Contact Form Submission - ADMIRERX
 
 Template Content:
@@ -49,21 +49,19 @@ Template Variables:
 1. Go to https://www.emailjs.com/ and create an account
 2. Create a new Email Service:
    - Service Type: Gmail
-   - Service Name: service_admirerx
+   - Service Name: service_n9ihigq
    - Connect your Gmail account (devr01499@gmail.com)
 
 3. Create a new Email Template:
-   - Template Name: template_admirerx_contact
+   - Template Name: template_gkhcy08
    - Use the template content above
    - Set the template variables as shown
 
 4. Get your Public Key:
    - Go to Account > API Keys
-   - Copy your Public Key
+   - Copy your Public Key (x9Mlm_5Uv0iRxcxdN)
 
-5. Update the EMAILJS_CONFIG object above with your actual credentials
-
-6. The contact form will automatically send emails to devr01499@gmail.com
+5. The contact form will automatically send emails to devr01499@gmail.com
 */
 
 export const initializeEmailJS = () => {
